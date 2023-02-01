9ab4c7ddc76f7b13bccd60b5c686388e033b220b
Delvoid <delvoid.dev@gmail.com>
2023-02-01 23:42:34 +0000

    kyu8 rust, grasshopper check for factor

--- rust/src/kyu8/mod.rs
+++ rust/src/kyu8/mod.rs
@@ -1,5 +1,6 @@
 pub mod are_you_playing_banjo;
 pub mod count_by_x;
+pub mod grasshopper_check_for_factor;
 pub mod opposites_attract;
 pub mod parse_nice_int_from_char_problem;
 pub mod you_cant_code_under_pressure;

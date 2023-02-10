d8341eded0a72a50c096db39b257a83d89c121bb
Delvoid <delvoid.dev@gmail.com>
2023-02-10 10:15:40 +0000

    another version

--- typescript/kyu6/fine_the_unique_number.ts
+++ typescript/kyu6/fine_the_unique_number.ts
@@ -1,5 +1,10 @@
 export function findUniq(arr: number[]): number {
-  return arr.find((n) => arr.indexOf(n) === arr.lastIndexOf(n)) || 0;
+  // find the unique number
+  const unique = arr.filter((num, index, array) => {
+    return array.indexOf(num) === array.lastIndexOf(num);
+  });
+
+  return unique[0];
 }
 
 console.log(findUniq([1, 1, 1, 2, 1, 1]) === 2);
@@ -8,4 +13,8 @@ console.log(findUniq([0, 0, 0.55, 0, 0]) === 0.55);
 export function findUniq2(arr: Array<number>): number {
   arr = arr.sort();
   return arr[0] == arr[1] ? arr[arr.length - 1] : arr[0];
+}
+
+export function findUniq3(arr: Array<number>): number {
+  return arr.find((n) => arr.indexOf(n) === arr.lastIndexOf(n)) || 0;
 }

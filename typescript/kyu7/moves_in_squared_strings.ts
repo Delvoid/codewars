b219e26c99c92511ab63d748a47cf6c0ee373849
Delvoid <delvoid.dev@gmail.com>
2023-03-06 15:21:58 +0000

    kyu7 typescript moves in squared string

---
+++ typescript/kyu7/moves_in_squared_strings.ts
@@ -0,0 +1,14 @@
+export function vertMirror(strng: string) {
+  return strng
+    .split('\n')
+    .map((s) => s.split('').reverse().join(''))
+    .join('\n');
+}
+export function horMirror(strng: string) {
+  return strng.split('\n').reverse().join('\n');
+}
+export function oper(fct: (s: string) => string, s: string) {
+  return fct(s);
+}
+
+console.log(oper(vertMirror, 'hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu'));

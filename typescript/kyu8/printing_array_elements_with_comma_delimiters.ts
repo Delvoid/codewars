a6e4ed332d03f7db7d866600e64fe9462f0e492c
Delvoid <delvoid.dev@gmail.com>
2023-03-24 11:39:16 +0000

    whoops

--- typescript/kyu8/printing_array_elements_with_comma_delimiters.ts
+++ typescript/kyu8/printing_array_elements_with_comma_delimiters.ts
@@ -1,4 +1,6 @@
-export function printArray(array: number | string[]) {}
+export function printArray(array: any[]) {
+  return array.join();
+}
 
 console.log(printArray([2, 4, 5, 2]));
 console.log(printArray(['hello', 'this', 'is', 'an', 'array!']));

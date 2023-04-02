57f8e752db863b5e65e37f42e56ed1dd989fc15c
Delvoid <delvoid.dev@gmail.com>
2023-04-02 15:20:56 +0100

    kyu5 typescript primes in number

---
+++ typescript/kyu5/primes_in_number.ts
@@ -0,0 +1,20 @@
+export const primeFactors = (n: number): string => {
+  if (n <= 1) return `(${n})`;
+  let result = '';
+  for (let i = 2; i <= n; i++) {
+    let count = 0;
+    while (n % i === 0) {
+      n /= i;
+      count += 1;
+    }
+    if (count > 0) {
+      result += `(${i}`;
+      if (count > 1) {
+        result += `**${count}`;
+      }
+      result += `)`;
+    }
+  }
+
+  return result;
+};

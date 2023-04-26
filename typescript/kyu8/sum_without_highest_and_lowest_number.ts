846a401d41e5073f15a7e5a01a8262d0d19775ed
Delvoid <delvoid.dev@gmail.com>
2023-04-26 10:02:06 +0100

    imagine saving

--- typescript/kyu8/sum_without_highest_and_lowest_number.ts
+++ typescript/kyu8/sum_without_highest_and_lowest_number.ts
@@ -2,3 +2,5 @@ export function sumArray(array:number[] | null) : number {
     const sorted = array?.sort((a, b) => a - b) ?? [];
     return sorted.slice(1, sorted.length - 1).reduce((a, b) => a + b, 0);
 }
+
+console.log(sumArray([ 6, 2, 1, 8, 10 ]));

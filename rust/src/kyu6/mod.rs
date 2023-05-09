438f74dd0556c4e78207864ffd6c6be0e364e38d
Delvoid <delvoid.dev@gmail.com>
2023-05-09 12:26:10 +0100

    kyu6 rust stop gninnnips

--- rust/src/kyu6/mod.rs
+++ rust/src/kyu6/mod.rs
@@ -3,6 +3,7 @@ pub mod create_phone_number;
 pub mod dashatize_it;
 pub mod multiples_of_3_or_5;
 pub mod split_strings;
+pub mod stop_gninnips_my_sdrow;
 pub mod sum_of_digits_digital_root;
 pub mod take_a_number_and_sum_its_digits_raised_to_the_cons_power;
 pub mod who_likes_it;
---
+++ rust/src/kyu6/stop_gninnips_my_sdrow.rs
@@ -0,0 +1,38 @@
+pub fn spin_words(words: &str) -> String {
+    words
+        .split_whitespace()
+        .map(|word| {
+            if word.len() >= 5 {
+                word.chars().rev().collect::<String>()
+            } else {
+                word.to_string()
+            }
+        })
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_spin_words() {
+        assert_eq!(spin_words("Welcome"), "emocleW");
+        assert_eq!(spin_words("Hey fellow warriors"), "Hey wollef sroirraw");
+        assert_eq!(spin_words("This is a test"), "This is a test");
+        assert_eq!(spin_words("This is another test"), "This is rehtona test");
+        assert_eq!(
+            spin_words("You are almost to the last test"),
+            "You are tsomla to the last test"
+        );
+        assert_eq!(
+            spin_words("Just kidding there is still one more"),
+            "Just gniddik ereht is llits one more"
+        );
+        assert_eq!(
+            spin_words("Seriously this is the last one"),
+            "ylsuoireS this is the last one"
+        );
+    }
+}

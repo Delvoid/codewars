599ee15e776cbb897e532ca4639c81041b8e7812
Delvoid <delvoid.dev@gmail.com>
2023-05-31 14:23:13 +0100

    clock me

--- rust/src/kyu8/beginner_series_2_clock.rs
+++ rust/src/kyu8/beginner_series_2_clock.rs
@@ -2,6 +2,6 @@ pub fn past(h: i32, m: i32, s: i32) -> i32 {
     ((h * 60 * 60) + (m * 60) + s) * 1000
 }
 
-fn past_2(h: i32, m: i32, s: i32) -> i32 {
+pub fn past_2(h: i32, m: i32, s: i32) -> i32 {
     h * 3600000 + m * 60000 + s * 1000
 }
